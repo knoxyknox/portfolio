e3db9303e47c7124a965f07a34a332a7888d1277
knox <kknoxy@yahoo.com>
2025-07-09 00:01:28 +0100

    added is sending state

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,11 +4,13 @@ import Swal from 'sweetalert2';
 
 const ContactForm = () => {
   const [isActive, setIsActive] = useState(false);
+  const [isSending, setIsSending] = useState(false);
   const handleClicked = () => {
     setIsActive(prev => !prev);
   };
   const onSubmit = async event => {
     event.preventDefault();
+    setIsSending(true);
     const formData = new FormData(event.target);
 
     formData.append('access_key', '9cf8681a-0f00-4b9e-ad9f-949ce4f08a98');
@@ -24,13 +26,16 @@ const ContactForm = () => {
       },
       body: json,
     }).then(res => res.json());
-
+    setIsSending(false);
     if (res.success) {
       Swal.fire({
         title: 'Success!',
         text: 'Message sent successfully!',
         icon: 'success',
       });
+      //reset the form and isActive state
+      event.target.reset();
+      setIsActive(false);
     }
   };
   return (
@@ -66,8 +71,12 @@ const ContactForm = () => {
             required
           ></textarea>
         </div>
-        <button onClick={handleClicked} type='submit' className={`btn ${isActive ? 'active':''}`}>
-          Send Message
+        <button
+          onClick={handleClicked}
+          type='submit'
+          className={`btn ${isActive ? 'active' : ''}`}
+        >
+          {isSending ? 'Sending...' : 'Send Message'}
         </button>
       </form>
     </div>
